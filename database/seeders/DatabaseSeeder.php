44117487a8deeff2f4f4c9688ff0f33c941e6eeb
GraceGenuardi <graziagenuardi96@gmail.com>
2023-04-27 15:03:47 +0200

    ADD TagSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             PostSeeder::class,
             CategorySeeder::class,
+            TagSeeder::class,
             PostSeeder::class,
         ]);
     }
