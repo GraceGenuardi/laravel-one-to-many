17700c3d3125218da72911f8c01ecacb44e66a2c
GraceGenuardi <graziagenuardi96@gmail.com>
2023-04-27 14:46:05 +0200

    Edit Controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\StorePostRequest;
 use App\Http\Requests\UpdatePostRequest;
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
